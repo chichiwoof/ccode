7258845fd16c5737bef9334f833c8eb7a1082b68
Chi <babu@Matus-iMac.local>
2020-03-25 14:25:10 -0700

    create adder and update mainargs

---
+++ adder/adder.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+    int     numberOfArgs = argc;
+    char *arg1 = argv[0];
+
+    int a = atoi(argv[1]);  
+    int b = atoi(argv[2]); 
+
+    int total = (a+b);
+
+    // printf("Number of Arguments: %d\n", numberOfArgs);
+    // printf("The program name: %s\n", arg1);
+    // printf("Argument 2 is the command line argument: %s\n", arg2);
+    // printf("Arg3 is second digit: %s\n", arg3);
+    printf("%i + %i = %i\n\n", a, b, total);
+    
+    return 0;
+ 
+}
